5537d33f0ecf61bf8e146e489ae77e20ae9db227
edgarjdq <edgarjdq@gmail.com>
2020-06-03 15:34:43 -0300

    Added constants + Added calculate score

--- src/main/kotlin/Score.kt
+++ src/main/kotlin/Score.kt
@@ -1,31 +1,50 @@
-enum class Score(val type: String) {
-    LOVE_ALL("Love-All"),
-    FIFTEEN_ALL("Fifteen-All"),
-    THIRTY_ALL("Thirty-All"),
-    ADVANTAGE_PLAYER_1("Advantage player1"),
-    ADVANTAGE_PLAYER_2("Advantage player2"),
-    WIN_PLAYER_1("Win for player1"),
-    WIN_PLAYER_2("Win for player2"),
-    DEUCE("Deuce");
-
-
+class Score {
     companion object {
-        fun calculateTied(score1: Int, score2: Int): Score {
-            return when(score1) {
-                0 ->  LOVE_ALL
+        const val LOVE_ALL = "Love-All"
+        const val FIFTEEN_ALL = "Fifteen-All"
+        const val THIRTY_ALL = "Thirty-All"
+
+        const val ADVANTAGE_PLAYER_1 = "Advantage player1"
+        const val ADVANTAGE_PLAYER_2 = "Advantage player2"
+        const val WIN_PLAYER_1 = "Win for player1"
+        const val WIN_PLAYER_2 = "Win for player2"
+
+        const val DEUCE = "Deuce"
+        const val LOVE = "Love"
+        const val FIFTEEN = "Fifteen"
+        const val THIRTY = "Thirty"
+        const val FORTY = "Forty"
+
+        fun calculateTied(score1: Int, score2: Int): String {
+            return when (score1) {
+                0 -> LOVE_ALL
                 1 -> FIFTEEN_ALL
                 2 -> THIRTY_ALL
                 else -> DEUCE
             }
         }
 
-        fun calculateWinCondition(score1: Int, score2: Int): Score {
+        fun calculateWinCondition(score1: Int, score2: Int): String {
             val minusResult = score1 - score2
             return when {
-                minusResult == 1 -> Score.ADVANTAGE_PLAYER_1
-                minusResult == -1 -> Score.ADVANTAGE_PLAYER_2
-                minusResult >= 2 -> Score.WIN_PLAYER_1
-                else -> Score.WIN_PLAYER_2
+                minusResult == 1 -> ADVANTAGE_PLAYER_1
+                minusResult == -1 -> ADVANTAGE_PLAYER_2
+                minusResult >= 2 -> WIN_PLAYER_1
+                else -> WIN_PLAYER_2
+            }
+        }
+
+        fun calculateScores(score1: Int, score2: Int): String {
+            return getValue(score1) + "-" + getValue(score2)
+        }
+
+        fun getValue(score: Int): String {
+            return when (score) {
+                0 -> LOVE
+                1 -> FIFTEEN
+                2 -> THIRTY
+                3 -> FORTY
+                else -> throw Exception()
             }
         }
     }
--- src/main/kotlin/TennisGame1.kt
+++ src/main/kotlin/TennisGame1.kt
@@ -5,35 +5,17 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     private var m_score2: Int = 0
 
     override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
+        if (playerName === player1Name)
             m_score1 += 1
         else
             m_score2 += 1
     }
 
-    override fun getScore(): String {
-        var score = ""
-        var tempScore = 0
-        if (m_score1 == m_score2) {
-            score = Score.calculateTied(m_score1, m_score2).type
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            score = Score.calculateWinCondition(m_score1, m_score2).type
-        } else {
-            for (i in 1..2) {
-                if (i == 1)
-                    tempScore = m_score1
-                else {
-                    score += "-"
-                    tempScore = m_score2
-                }
-                when (tempScore) {
-                    0 -> score += "Love"
-                    1 -> score += "Fifteen"
-                    2 -> score += "Thirty"
-                    3 -> score += "Forty"
-                }
-            }
-        }
-        return score
+    override fun getScore(): String = if (m_score1 == m_score2) {
+        Score.calculateTied(m_score1, m_score2)
+    } else if (m_score1 >= 4 || m_score2 >= 4) {
+        Score.calculateWinCondition(m_score1, m_score2)
+    } else {
+        Score.calculateScores(m_score1, m_score2)
     }
 }
